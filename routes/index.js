5a002dcf2c8cd44df61031d99aff7893780685dd
Craig5117 <craig.bennett5117@gmail.com>
2021-03-10 09:05:49 -0500

    fixed typos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
   });
 
+router.post('/api/send_email', function(req, res) {
+  res.set("Content-Type", "application/json");
+
+  /* Send email here */
+
+  res.send('{"message":"Email sent."}');
+});
+
 module.exports = router;
