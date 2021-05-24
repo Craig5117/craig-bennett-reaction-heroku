51904986b46c9d035362fa32f55e0078a21d0165
Craig5117 <craig.bennett5117@gmail.com>
2021-05-24 15:05:13 -0400

    attempting to fix too many redirects

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ app.use(express.urlencoded({ extended: true }));
   // Handle React routing, return all requests to React app
 
   app.get('*', function(req, res) {
-    if (req.headers.host === 'craigbennett-reaction.herokuapp.com'){
-      return res.redirect(301, path.join(__dirname, 'client/build', 'index.html'));
-    }
     if (req.headers['x-forwarded-proto'] !== 'https') {
       console.log("Redirecting to https")
       return res.redirect('https://' + path.join(__dirname, 'client/build', 'index.html'));
