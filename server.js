f343444d6424c0c8a7af4c63ff3e74100f429f80
Craig5117 <craig.bennett5117@gmail.com>
2021-05-24 15:09:14 -0400

    added next to parameters

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
     app.use(routes);
   // Handle React routing, return all requests to React app
 
-  app.get('*', function(req, res) {
+  app.get('*', function(req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       console.log("Redirecting to https")
       return res.redirect('https://' + path.join(__dirname, 'client/build', 'index.html'));
