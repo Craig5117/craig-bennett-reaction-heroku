e3f3f085ddf5800020d6ee13ff93e5b6fb2b9931
Craig5117 <craig.bennett5117@gmail.com>
2021-05-24 14:32:58 -0400

    turning off wildcard route

--- server.js
+++ server.js
@@ -32,20 +32,20 @@ app.use(express.urlencoded({ extended: true }));
     app.use(routes);
   // Handle React routing, return all requests to React app
 
-  app.get('*', function(req, res) {
-    console.log(req.headers)
-    console.log("Hi Craig!!!")
-    if (req.headers.host === 'craigbennett-reaction.herokuapp.com')
-        return res.redirect(301, 'craigbennett-reaction.herokuapp.com' + path.join(__dirname, 'client/build', 'index.html'));
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      console.log(req.headers)
-      return res.redirect('https://' + path.join(__dirname, 'client/build', 'index.html'));
-    } 
-    else {
-      return next();
-    }
+//   app.get('*', function(req, res) {
+//     console.log(req.headers)
+//     console.log("Hi Craig!!!")
+//     if (req.headers.host === 'craigbennett-reaction.herokuapp.com')
+//         return res.redirect(301, 'craigbennett-reaction.herokuapp.com' + path.join(__dirname, 'client/build', 'index.html'));
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       // console.log(req.headers)
+//       return res.redirect('https://' + path.join(__dirname, 'client/build', 'index.html'));
+//     } 
+//     else {
+//       return next();
+//     }
         
-});
+// });
     // app.get('*', function(req, res) {
     //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     // });
